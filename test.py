2dcf42e50c74175e0ccc48e1d06e3fe32c7079c4
vincent51689453 <vincent51689453@gmail.com>
2020-12-02 11:06:12 +0800

    clear test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from scipy.signal import argrelextrema
-
-    
-    
-a = np.array([10,10,10,2,10,10,10,10,10,2,10,10,10,2,10,10,10,13])
-print(a[-1])
-b = np.array([])
-print(b.shape[0])
